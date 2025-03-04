951076e1959eec056c7c28e3db281b8754140131
Suraj Acharya <acharyasuraj091@gmail.com>
2025-03-04 13:51:10 -0500

    Chaning the README file and the add a template to the heat equation solution

--- README.md
+++ README.md
@@ -3,3 +3,19 @@ Examples for solving pde. Undergraduate research.
 
 
 ### Research
+
+
+### Heat Equation
+
+The Heat Equation Solution approximates the solution to the parabolic partial differential equation (PDE) given by:
+```math
+\frac{\partial u}{\partial t}(x,t) - \alpha^2 \frac{\partial^2 u}{\partial x^2}(x,t) = 0, \quad 0 < x < l,\quad  0 < t < T,
+```
+subject to the boundary conditions
+```math
+u(0, t)= u(l, t) = 0, \quad 0 < t < T,
+```
+and the initial conditions
+```math
+u(x, 0) = f(x), \quad 0 \leq x \leq l.
+```
---
+++ heat_equation_backward_difference.py
@@ -0,0 +1,29 @@
+
+
+
+def heat_equation_backward_difference():
+    """
+    Solve the heat equation using the backward difference method.
+        
+    INPUT:
+    - l: End Point
+    - T: Maximum Time
+    - alpha: Constant
+    - m: `>=` 3
+    - N: `>=` 1
+    - f: Boundary condition such that `f(x) = u(x, 0)`
+    
+    OUTPUT:
+    - Approxiamtions `w_{i,j}` to `u(x_i, t_j)` for each `i = 1,..., m-1` and `j = 1,..., N`
+    """
+    pass 
+
+if __name__ == "__main__":
+    heat_equation_backward_difference()
+    
+    
+
+
+
+
+
